c9b50d420a113fc72e2566618bfdb7896b8b1eda
DK\mao <mao@sitecore.net>
2017-10-27 16:38:44 +0300

    Utilize dynamic search API instead of fast query when setting presentation details on standard values

---
+++ src/Sitecore.Support.190792/Data/Items/ItemUtil.cs
@@ -0,0 +1,123 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq;
+using Sitecore.ContentSearch.Linq.Utilities;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Sitecore.Support.Data.Items
+{
+  public static class ItemUtil
+  {
+    public static void SetLayoutDetails(Item item, string sharedLayout, string finalLayout)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      Assert.ArgumentNotNull(sharedLayout, "sharedLayout");
+      Assert.ArgumentNotNull(finalLayout, "finalLayout");
+      string text = sharedLayout + finalLayout;
+
+      sharedLayout = InvokeItemUtilCleanupLayoutValue(sharedLayout) as string;
+      finalLayout = InvokeItemUtilCleanupLayoutValue(finalLayout) as string;
+      using (new StatisticDisabler(StatisticDisablerState.ForItemsWithoutVersionOnly))
+      {
+        item.Editing.BeginEdit();
+        Field field = item.Fields[FieldIDs.LayoutField];
+        string xml = InvokeItemUtilCleanupLayoutValue(LayoutField.GetFieldValue(field)) as string;
+        if (!XmlUtil.XmlStringsAreEqual(xml, sharedLayout))
+        {
+          LayoutField.SetFieldValue(field, sharedLayout);
+        }
+        if (!item.RuntimeSettings.TemporaryVersion)
+        {
+          Field field2 = item.Fields[FieldIDs.FinalLayoutField];
+          LayoutField.SetFieldValue(field2, finalLayout, sharedLayout);
+        }
+        item.Editing.EndEdit();
+      }
+      if (item.Name == "__Standard Values")
+      {
+        Sitecore.Support.Data.Items.ItemUtil.CleanupInheritedItems(item);
+      }
+      Log.Audit(typeof(ItemUtil), "Set layout details: {0}, layout: {1}", new string[]
+      {
+        AuditFormatter.FormatItem(item),
+        text
+      });
+    }
+
+    private static void CleanupInheritedItems(Item item)
+    {
+      Item[] array = GetInheritedItems(item);
+      if (array == null)
+      {
+        return;
+      }
+      using (new StatisticDisabler(StatisticDisablerState.ForItemsWithoutVersionOnly))
+      {
+        Item[] array2 = array;
+        for (int i = 0; i < array2.Length; i++)
+        {
+          Item item2 = array2[i];
+          Field field = item2.Fields[FieldIDs.LayoutField];
+          Field field2 = item2.Fields[FieldIDs.FinalLayoutField];
+          using (new EditContext(item2))
+          {
+            if (field.HasValue)
+            {
+              string text = LayoutField.GetFieldValue(field);
+              text = InvokeItemUtilCleanupLayoutValue(text) as string;
+              LayoutField.SetFieldValue(field, text);
+            }
+            if (field2.HasValue)
+            {
+              string text2 = LayoutField.GetFieldValue(field2);
+              text2 = InvokeItemUtilCleanupLayoutValue(text2) as string;
+              LayoutField.SetFieldValue(field2, text2);
+            }
+          }
+        }
+      }
+    }
+
+    private static Item[] GetInheritedItems(Item item)
+    {
+      var path = "/sitecore/content";
+      Item[] items = new Item[0];
+      using (IProviderSearchContext searchContext = ContentSearchManager.GetIndex($"sitecore_{item.Database.Name}_index").CreateSearchContext())
+      {
+        var predicate = PredicateBuilder.True<SearchResultItem>()
+          .And(searchItem => searchItem.TemplateId == item.TemplateID);
+
+        var searchItems = searchContext.GetQueryable<SearchResultItem>()
+         .Where(predicate)
+         .Where(searchItem => searchItem["_latestversion"].Equals("1"))
+         .Filter(searchItem => searchItem.Path.StartsWith(path))
+         .Filter(searchItem => searchItem.Language == Sitecore.Context.Language.Name).ToArray();
+
+        items = new Item[searchItems.Count()];
+        for (int i = 0; i < searchItems.Count(); i++)
+        {
+          items[i] = item.Database.GetItem(searchItems[i].ItemId);
+        }
+      }
+
+      return items;
+    }
+
+    private static object InvokeItemUtilCleanupLayoutValue(params object[] parameters)
+    {
+      var itemUtilType = typeof(Sitecore.Data.Items.ItemUtil);
+      var flags = BindingFlags.NonPublic | BindingFlags.Static;
+
+      return itemUtilType.GetMethod("CleanupLayoutValue", flags).Invoke(null, parameters);
+    }
+  }
+}
---
+++ src/Sitecore.Support.190792/Shell/Framework/Commands/SetLayoutDetails.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Data.Items;
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Applications.Dialogs.LayoutDetails;
+using Sitecore.Configuration;
+using Sitecore.Globalization;
+using Sitecore.Text;
+
+namespace Sitecore.Support.Shell.Framework.Commands
+{
+  public class SetLayoutDetails : Sitecore.Shell.Framework.Commands.SetLayoutDetails
+  {
+    protected override void Run(ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (!SheerResponse.CheckModified(new CheckModifiedParameters
+      {
+        ResumePreviousPipeline = true
+      }))
+      {
+        return;
+      }
+      if (args.IsPostBack)
+      {
+        if (args.HasResult)
+        {
+          Database database = Factory.GetDatabase(args.Parameters["database"]);
+          Assert.IsNotNull(database, "Database \"" + args.Parameters["database"] + "\" not found.");
+          Item item = database.GetItem(ID.Parse(args.Parameters["id"]), Language.Parse(args.Parameters["language"]), Sitecore.Data.Version.Parse(args.Parameters["version"]));
+          Assert.IsNotNull(item, "item");
+          LayoutDetailsDialogResult layoutDetailsDialogResult = LayoutDetailsDialogResult.Parse(args.Result);
+          Sitecore.Support.Data.Items.ItemUtil.SetLayoutDetails(item, layoutDetailsDialogResult.Layout, layoutDetailsDialogResult.FinalLayout);
+          if (layoutDetailsDialogResult.VersionCreated)
+          {
+            Context.ClientPage.SendMessage(this, string.Concat(new object[]
+            {
+          "item:versionadded(id=",
+          item.ID,
+          ",version=",
+          item.Version,
+          ",language=",
+          item.Language,
+          ")"
+            }));
+            return;
+          }
+        }
+      }
+      else
+      {
+        UrlString urlString = new UrlString(UIUtil.GetUri("control:LayoutDetails"));
+        urlString.Append("id", args.Parameters["id"]);
+        urlString.Append("la", args.Parameters["language"]);
+        urlString.Append("vs", args.Parameters["version"]);
+        SheerResponse.ShowModalDialog(urlString.ToString(), "650px", string.Empty, string.Empty, true);
+        args.WaitForPostBack();
+      }
+    }
+  }
+}
